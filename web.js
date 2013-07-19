9be15fe8ce819a46faf6ef6f5cf7ae7ecac1ccd5
Ubuntu <ubuntu@ip-172-31-31-62.ap-southeast-2.compute.internal>
2013-07-19 14:02:16 +0000

    twitter bootstrap combined

--- web.js
+++ web.js
@@ -3,13 +3,15 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var index_file_buf = fs.readFileSync("index.html");
+// to make the index.html everytime the request comes in
+// var index_file_buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
+  var index_file_buf = fs.readFileSync("index.html");
   response.send(index_file_buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
